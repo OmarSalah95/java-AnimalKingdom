f3c3eb29cef9b6ac271094a38f6b7529b11c2183
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-08-07 12:17:22 -0700

    Finished writing animal abstract working on sub classes

---
+++ src/Kingdom/AbstractAnimal.java
@@ -0,0 +1,48 @@
+package Kingdom;
+
+public abstract class AbstractAnimal {
+    private static int maxId;
+
+    private int id = 0;
+    private String name;
+    private int discoveryYear;
+
+    public AbstractAnimal(String name, int year) {
+        id = maxId++;
+        this.name = name;
+        this.discoveryYear = year;
+    }
+
+    // move
+    public abstract String getMovement();
+
+    // breath
+    public abstract String getBreath();
+
+    // reproduce
+    public abstract String getReproduction();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDiscoveryYear() {
+        return discoveryYear;
+    }
+
+    public void setDiscoveryYear(int discoveryYear) {
+        this.discoveryYear = discoveryYear;
+    }
+}
